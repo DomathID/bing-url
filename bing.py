07074921d455a69c2f69faee14587571343e1bf1
Bang Domath ID <bangdompret@gmail.com>
2023-09-22 01:01:00 +0700

    Update bing.py
    Update :
    - Info Kuota Harian dan Bulanan
    - Submit Batch
    - Submit Single
    - baca Api dari file api.txt

--- bing.py
+++ bing.py
@@ -1,25 +1,46 @@
-import base64
-import json
-import re
-import requests
+def main_menu():
+    banner = """
+    ██████╗ ██╗███╗   ██╗██████╗ ███████╗██╗  ██╗
+    ██╔══██╗██║████╗  ██║██╔══██╗██╔════╝╚██╗██╔╝
+    ██████╔╝██║██╔██╗ ██║██║  ██║█████╗   ╚███╔╝ 
+    ██╔══██╗██║██║╚██╗██║██║  ██║██╔══╝   ██╔██╗ 
+    ██████╔╝██║██║ ╚████║██████╔╝███████╗██╔╝ ██╗
+    ╚═════╝ ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+       """
+    print(banner)
+    print(colored("Your IP: ", "blue"), colored(ip, "green"))
+    print(colored("Author:", "blue"), colored("DomathID", "green"))
+    print(colored("Github:", "blue"), colored("https://github.com/DomathID", "green"))
+    print("\nMenu Utama:")
+    print(colored("[1]", "magenta"), "Submit URL Single")
+    print(colored("[2]", "magenta"), "Submit URL dalam Batch")
+    print(colored("[3]", "magenta"), "Cek Kuota")
+    print(colored("[4]", "magenta"), "Keluar")
 
-## Bing Info
-bing_submission_url = "https://ssl.bing.com/webmaster/api.svc/json/SubmitUrlbatch?apikey="
-print(r"""
-░█▀▄░▀█▀░█▀█░█▀▀░█▀▀░█░█░█▀▄░█░░
-░█▀▄░░█░░█░█░█░█░█░░░█░█░█▀▄░█░░
-░▀▀░░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀
-""")
-print("            Submit URL Bing.com ")
-x = 'Enter Api: '
-bing_api_key =  input(x)
-r = 'Enter URL:'
-l = 'Url List: '
-bing_submission_urls = { "siteUrl": input(r), "urlList": [input(l)]} #CreateURL list to submit to Bing.
-headers = { "Content-Type": "application/json; charset=utf-8","Host": "ssl.bing.com" } #Bing response headers.
-## Make request to Bing.
-submission_request = requests.post(f"{bing_submission_url}{bing_api_key}", headers=headers, json=bing_submission_urls)
-if submission_request.status_code == 200:
-	print("Submission to Bing was successful.")
-else:
-	print("Submission was not successful. Please try again.")
+    choice = input("Pilih opsi (1/2/3/4): ")
+    if choice == "1":
+        api_key = read_api_key()
+        site_url = input("URL situs: ")
+        page_url = input("Page Situs: ")
+        submit_single_url(api_key, site_url, page_url)
+    elif choice == "2":
+        api_key = read_api_key()
+        site_url = input("Masukkan URL situs: ")
+        url_list = []
+        count = 1
+        while True:
+            url = input(f"Masukkan URL {count} (kosongkan untuk selesai): ")
+            if url == "":
+                break
+            url_list.append(url)
+            count += 1
+        submit_batch_urls(api_key, site_url, url_list)
+    elif choice == "3":
+        api_key = read_api_key()
+        site_url = input("URL situs: ")
+        get_quota(api_key, site_url)
+    elif choice == "4":
+        print("Keluar dari Tools")
+        sys.exit(0)
+    else:
+        print(colored("Opsi tidak valid. Silakan pilih opsi yang valid.", "yellow"))

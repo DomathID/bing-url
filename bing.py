e76299e1d2ae0c7944b27eb1586695ecf636a0ce
Bang Domath ID <bangdompret@gmail.com>
2023-09-24 14:12:51 +0700

    Update bing.py
    Maaf kepotong pas paste 🙏, sekarang sudah fix

--- bing.py
+++ bing.py
@@ -1,3 +1,56 @@
+import requests
+import json
+import os
+import sys
+from termcolor import colored
+
+ip = requests.get("https://api.ipify.org").text
+
+
+def submit_single_url(api_key, site_url, page_url):
+    url = f"https://ssl.bing.com/webmaster/api.svc/json/SubmitUrl?apikey={api_key}"
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    data = {"siteUrl": site_url, "url": page_url}
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        print(colored("\nBerhasil Dikirim", "green"))
+    else:
+        print(colored("Gagal mengirim permintaan.", "red"))
+
+
+def submit_batch_urls(api_key, site_url, url_list):
+    url = f"https://ssl.bing.com/webmaster/api.svc/json/SubmitUrlbatch?apikey={api_key}"
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    data = {"siteUrl": site_url, "urlList": url_list}
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        print(colored("\nBerhasil Dikirim", "green"))
+    else:
+        print(colored("Gagal mengirim permintaan.", "red"))
+
+
+def read_api_key():
+    try:
+        with open("api.txt", "r") as file:
+            api_key = file.read().strip()
+            return api_key
+    except FileNotFoundError:
+        print("File api.txt tidak ditemukan.")
+        sys.exit(1)
+
+
+def get_quota(api_key, site_url):
+    url = f"https://ssl.bing.com/webmaster/api.svc/json/GetUrlSubmissionQuota?siteUrl={site_url}&apikey={api_key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        quota_data = response.json().get("d")
+        daily_quota = quota_data.get("DailyQuota")
+        monthly_quota = quota_data.get("MonthlyQuota")
+        print(f"Kuota Harian: {daily_quota}")
+        print(f"Kuota Bulanan: {monthly_quota}")
+    else:
+        print(colored("Gagal mendapatkan kuota.", "red"))
+
 def main_menu():
     banner = """
     ██████╗ ██╗███╗   ██╗██████╗ ███████╗██╗  ██╗
@@ -44,3 +97,4 @@ def main_menu():
         sys.exit(0)
     else:
         print(colored("Opsi tidak valid. Silakan pilih opsi yang valid.", "yellow"))
+main_menu()
